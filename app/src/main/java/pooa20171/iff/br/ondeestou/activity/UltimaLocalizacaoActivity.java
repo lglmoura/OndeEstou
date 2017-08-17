8d2ca4f7fed1752c275a75ff3232134629a12903
Luiz Gustavo <lglmoura@gmail.com>
2017-08-17 17:13:29 -0300

    Incluido os atributos e implementa o metodo callConnection()

--- app/src/main/java/pooa20171/iff/br/ondeestou/activity/UltimaLocalizacaoActivity.java
+++ app/src/main/java/pooa20171/iff/br/ondeestou/activity/UltimaLocalizacaoActivity.java
@@ -5,10 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import pooa20171.iff.br.ondeestou.R;
 
@@ -16,10 +20,41 @@ public class UltimaLocalizacaoActivity extends AppCompatActivity implements Goog
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
 
+
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+
+    private TextView tvLatitude;
+    private TextView tvLongitude;
+    private TextView tvAltitude;
+    private TextView tvVelocidade;
+    private TextView tvProvedor;
+    private TextView tvPresicao;
+    private TextView tvHora;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultima_localizacao);
+        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
+        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
+        tvAltitude = (TextView) findViewById(R.id.tvAltitude);
+        tvPresicao = (TextView) findViewById(R.id.tvPrecisao);
+        tvVelocidade = (TextView) findViewById(R.id.tvVelocidade);
+        tvProvedor = (TextView) findViewById(R.id.tvProvedor);
+        tvHora = (TextView) findViewById(R.id.tvHora);
+    }
+
+    private synchronized void callConnection() {
+        Log.i("LOG", "callConnection()");
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        //googleApiClient.connect();
     }
 
     @Override
